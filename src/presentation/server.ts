9eedaee25507058ab448b92f13bc105b9ce26562
Daniel Restrepo <jdrestrepo@unitecnica.net>
2024-04-06 13:56:15 -0500

    Create check service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
 import { CronService } from "./cron/cron-service";
 
 
@@ -7,10 +8,9 @@ export class Server {
         console.log('Server started...');
        
         CronService.createJob(
-            '*/5 * * * * *',
+            '*/5 * * * * *', 
             () => {
-                const date = new Date();
-                console.log('5 seconds', date)
+                new CheckService().execute( 'https://google.com' )
             }
         )
     }
