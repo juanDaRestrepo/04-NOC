efeab0825adccb10991f05d45f0995d63302d14f
Daniel Restrepo <jdrestrepo@unitecnica.net>
2024-04-06 11:52:27 -0500

    Create adapter cron-service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,17 @@
+import { CronService } from "./cron/cron-service";
+
 
 
 export class Server {
     static start() {
         console.log('Server started...');
+       
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const date = new Date();
+                console.log('5 seconds', date)
+            }
+        )
     }
 }
