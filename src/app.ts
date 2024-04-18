1520cc1242a67280e09b58f5a0698c95bf67176a
Daniel Restrepo <jdrestrepo@unitecnica.net>
2024-04-18 16:57:31 -0500

    Create docker-compose.yml for creating mongo instance

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,11 @@
 
-import { Console } from "console";
+
 import { Server } from "./presentation/server";
-import { envs } from "./config/plugins/envs.pulgin";
-
-
 
 (async() => {
     main();
 })();
 
-
 function main() {
     Server.start();
-    /* console.log(envs.PORT) */
 }
