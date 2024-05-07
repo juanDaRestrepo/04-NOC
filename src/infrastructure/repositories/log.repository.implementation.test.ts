9a6073f71d99bf07701bbbb14a02b49ce9bc4eef
Daniel Restrepo <juanrestrepowebdev@gmail.com>
2024-05-06 20:26:13 -0500

    Test log.repository.implementation

---
+++ src/infrastructure/repositories/log.repository.implementation.test.ts
@@ -0,0 +1,27 @@
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+import { LogRepositoryImpl } from "./log.repository.implementation";
+
+
+describe('Log.repository.implementation', () => {
+    const mockLogDatasource = {
+        saveLog: jest.fn(),
+        getLogs: jest.fn(),
+    };
+
+    const logRepository = new LogRepositoryImpl(mockLogDatasource);
+    
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+
+    test('saveLog should call the datasource with arguments', async() => {
+        const log = { level: LogSeverityLevel.high, message: 'hola'} as LogEntity;
+        await logRepository.saveLog(log);
+        expect( mockLogDatasource.saveLog).toHaveBeenCalledWith( log );
+    });
+
+    test('getLogs should call the datasource with arguments', async() => {
+        await logRepository.getLogs(LogSeverityLevel.medium);
+        expect( mockLogDatasource.getLogs).toHaveBeenCalledWith( LogSeverityLevel.medium );
+    });
+})
